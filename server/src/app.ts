1c66a376db68644185f1f00a61d487849c1adf23
Destaw <destawgm@gmail.com>
2025-12-30 15:25:20 +0200

    updates for prod

--- server/src/app.ts
+++ server/src/app.ts
@@ -71,6 +71,8 @@ app.use(morgan('combined'));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
+app.set("trust proxy", 1);
+
 
 app.get('/health', (req, res) => {
   res.status(200).json({
@@ -81,7 +83,6 @@ app.get('/health', (req, res) => {
   });
 });
 
-app.get("/health", (req, res) => res.status(200).send("OK"));
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/groups', authenticateToken, groupRoutes);
