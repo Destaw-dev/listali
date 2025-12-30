4fc6f9de6b3ca1e205adb1baa9e663e2f259043c
Destaw <destawgm@gmail.com>
2025-12-30 03:38:58 +0200

    updates

--- client/src/__tests__/pages/ShoppingListPage.test.tsx
+++ client/src/__tests__/pages/ShoppingListPage.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { renderWithProviders } from '@/test/test-utils';
 import ShoppingListPage from '@/app/[locale]/groups/[groupId]/[listId]/page';
 import { useShoppingListData } from '@/hooks/useShoppingListData';
